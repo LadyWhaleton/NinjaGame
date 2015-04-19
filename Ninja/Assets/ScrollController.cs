ad8b77827919a082d59d4a22d5012e07efcfe4ff
ryosaito420 <ryocsaito@gmail.com>
2015-04-19 02:02:27 -0700

    add being retarded

---
+++ Ninja/Assets/ScrollController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollController : MonoBehaviour {
+
+
+	public int ninjasSafe= 0;
+	public int winNumber;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (ninjasSafe == winNumber) {
+			print("GAME WON");
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		if (coll.gameObject.tag == "ninja") {
+			++ninjasSafe;
+		}
+
+
+	}
+
+}
